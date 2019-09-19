73e46eb81161aed6aa182b7bedadd00404e9a60d
lukwiya23 <luk23bonnie8@gmail.com>
2019-09-19 15:18:49 +0300

    setting up the game colors

---
+++ classes/game.py
@@ -0,0 +1,9 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[95m'
+    OKGREEN = '\033[95m'
+    WARNING = '\033[95m'
+    FAIL = '\033[95m'
+    ENDC = '\033[95m'
+    BOLD = '\033[95m'
+    UNDERLINE = '\033[95m'
